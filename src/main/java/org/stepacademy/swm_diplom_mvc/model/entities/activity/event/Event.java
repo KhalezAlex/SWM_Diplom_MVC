b805b7d32f1267f7435561fbe172686fa3239338
Святослав Киреев <svytkireev@icloud.com>
2023-04-02 13:16:16 +0700

    DONE: - Добавлен поиск инициатора в admin-event по никнейму; - в pom вынес версии библиотек; - класс Event отредактировал по плагину CheckStyle. TODO: - отредактировать проект по плагину CheckStyle; - Добавить отображение созданных мероприятий с двух сторон; - Добавить редактирование ивентов, создателем; - Протестировать функционал приложения; - UT5 желательно бы :)

--- src/main/java/org/stepacademy/swm_diplom_mvc/model/entities/activity/event/Event.java
+++ src/main/java/org/stepacademy/swm_diplom_mvc/model/entities/activity/event/Event.java
@@ -2,6 +2,9 @@ package org.stepacademy.swm_diplom_mvc.model.entities.activity.event;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,9 +12,6 @@ import org.stepacademy.swm_diplom_mvc.model.entities.activity.activity.Activity;
 import org.stepacademy.swm_diplom_mvc.model.entities.customer.customer.Customer;
 import org.stepacademy.swm_diplom_mvc.model.entities.location.city.City;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -19,63 +19,60 @@ import java.util.Set;
 @Table(name = "event_t")
 @NoArgsConstructor
 public class Event {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
-    @ManyToOne
-    @JoinColumn(name = "activity_id")
-    private Activity activity;
+  @ManyToOne
+  @JoinColumn(name = "activity_id")
+  private Activity activity;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "city_id")
-    private City city;
+  @ManyToOne(cascade = CascadeType.MERGE)
+  @JoinColumn(name = "city_id")
+  private City city;
 
-    @Column(name = "address", nullable = false)
-    private String address;
+  @Column(name = "address", nullable = false)
+  private String address;
 
-    @Column(name = "dateTime", nullable = false)
-    private LocalDateTime dateTime;
+  @Column(name = "dateTime", nullable = false)
+  private LocalDateTime dateTime;
 
-//инициатор события. Используем Merge для корректной передачи Event из html
-    @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "initiator_id")
-    private Customer initiator;
+  public LocalDateTime getDateTime() {
+    return LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth(), dateTime.getHour(),
+            dateTime.getMinute());
+  }
 
-//участники события
-    @ManyToMany(mappedBy = "eventsIn", cascade = CascadeType.ALL)
-    @JsonIgnore
-    private Set<Customer> participants = new HashSet<>();
+  //инициатор события. Используем Merge для корректной передачи Event из html
+  @ManyToOne(cascade = CascadeType.MERGE)
+  @JoinColumn(name = "initiator_id")
+  private Customer initiator;
 
-    @Column(name = "needed")
-    private Integer needed;
+  //участники события
+  @ManyToMany(mappedBy = "eventsIn", cascade = CascadeType.ALL)
+  @JsonIgnore
+  private Set<Customer> participants = new HashSet<>();
 
-    @Column(name = "will_come")
-    private Integer willCome;
+  @Column(name = "needed")
+  private Integer needed;
+
+  @Column(name = "will_come")
+  private Integer willCome;
+
+  public Event(Activity activity, City city, String address, LocalDateTime dateTime, Customer initiator, int needed,
+               int willCome) {
+    this.activity = activity;
+    this.city = city;
+    this.address = address;
+    this.dateTime = dateTime;
+    this.initiator = initiator;
+    this.needed = needed;
+    this.willCome = willCome;
+  }
 
 
-
-    public Event(Activity activity, City city, String address, LocalDateTime dateTime,
-                 Customer initiator, int needed, int willCome) {
-        this.activity = activity;
-        this.city = city;
-        this.address = address;
-        this.dateTime = dateTime;
-        this.initiator = initiator;
-        this.needed = needed;
-        this.willCome = willCome;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "id=" + id +
-                ", activity=" + activity.getId() +
-                ", city=" + city.getId() +
-                ", address='" + address + '\'' +
-                ", dateTime=" + dateTime +
-                ", initiator=" + initiator.getId() +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Event{id= " + id + ", activity= " + activity.getId() + ", city= " + city.getId() + ", address= "
+              + address + ", dateTime= " + dateTime + ", initiator= " + initiator.getId() + '}';
+  }
 }
